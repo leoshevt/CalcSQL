7747940320de586b064080877c61bf4365a19acd
leoshevt <38049287+leoshevt@users.noreply.github.com>
2018-08-10 21:01:13 +0400

    Upgrade 0.1

--- Main.java
+++ Main.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        ConnectionToBase CTB = new ConnectionToBase();
         System.out.println("Welcome to calculator");
         System.out.println("To start working, press enter and write: number, number, operator");
         System.out.println("If you want exit, write: exit");
--- reader.java
+++ reader.java
@@ -1,17 +1,30 @@
 package com.company;
 
-public class reader {
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Reader {
     public static int firstarg;
     public static int secondarg;
     public static String operator;
 
-    public reader() {
+    public Reader() {
     }
 
-    void read(String a, String b, String c) {
-        this.firstarg = Integer.parseInt(a);
-        this.secondarg = Integer.parseInt(b);
-        this.operator = c;
+    void read() {
+        this.firstarg = new Scanner(System.in).nextInt();
+        this.secondarg = new Scanner(System.in).nextInt();
+        this.operator = new Scanner(System.in).nextLine();
+    }
+    void choise() throws SQLException, ClassNotFoundException {
+        String choise = new Scanner(System.in).nextLine();
+        switch (choise){
+            case "exit": break;
+            default:{Reader reader = new Reader();
+                reader.read();
+                new Calculator().calculate();
+                new ConnectionToBase().writeToBase(Integer.toString(Reader.firstarg), Integer.toString(Reader.secondarg), Reader.operator, Calculator.result);}
+        }
     }
 
 
